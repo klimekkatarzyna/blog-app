3d261401b7df1b527a9e66283f33556b2db43e18
Katarzyna Krawczyk <katarzynaklimek26@gmail.com>
2024-07-22 15:23:09 +0200

    create spinner and error / loading pages

---
+++ src/page/ErrorPage.tsx
@@ -0,0 +1,10 @@
+export const ErrorPage: React.FC = () => (
+  <div
+    className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-transparent"
+    data-testid="error-page"
+  >
+    <h1 className="text-gray-700 font-semibold text-xl">
+      Something went wrong
+    </h1>
+  </div>
+);
