9d575bc0bfc8b96564a52c2736d910fbc7874c47
Katarzyna Krawczyk <katarzynaklimek26@gmail.com>
2024-07-22 15:25:02 +0200

    main layout update

--- src/page/MainLayoutPage.tsx
+++ src/page/MainLayoutPage.tsx
@@ -1,33 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
-
 import { Header } from "../components/Header";
-import { List } from "../components/List";
-import { LoadingPage } from "./LoadingPage";
-import { getPosts } from "../services/posts";
 import { Button } from "../components/Button";
+import { Outlet } from "react-router-dom";
 
 export const MainLayoutPage: React.FC = () => {
-  const { error, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: getPosts,
-  });
-
-  if (isLoading) {
-    return <LoadingPage />;
-  }
-
-  if (error) {
-    return (
-      <div className="flex h-screen items-center justify-center text-slate-900">
-        Something went wrong
-      </div>
-    );
-  }
-
   return (
     <div className="relative">
       <Header />
-      <List />
+      <Outlet />
       <Button />
     </div>
   );
