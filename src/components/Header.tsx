b27ab8653290ab6af9b4d21d6853bc768d24afe2
Katarzyna Krawczyk <katarzynaklimek26@gmail.com>
2024-07-22 15:21:20 +0200

    add back button & select component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+
+import { fetchAutorById, fetchAutors } from "../services/autors";
+import { Select } from "./Select";
+import { BackButton } from "./BackButton";
+
 export const Header: React.FC = () => {
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["autors"],
+    queryFn: () => fetchAutors(),
+  });
+
+  const { userId, postId } = useParams();
+
+  const { data: selectedAutor } = useQuery({
+    queryKey: ["autors", userId],
+    queryFn: () => fetchAutorById(userId),
+    enabled: !!userId,
+  });
+
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full border-b text-sm py-2.5 sm:py-4 sticky top-0 bg-white/30 backdrop-blur-md">
+    <header className="flex flex-wrap sm:justify-start z-10 sm:flex-nowrap w-full border-b text-sm py-2.5 sm:py-4 sticky top-0 bg-white/30 backdrop-blur-md">
       <nav
-        className="max-w-7xl flex basis-full items-center w-full mx-auto px-4 sm:px-6 lg:px-8"
+        className="max-w-7xl flex basis-full items-center w-full mx-auto px-4 sm:px-6 lg:px-8 gap-4"
         aria-label="Global"
       >
-        <div className="w-full sm:w-auto">
-          <label htmlFor="icon" className="sr-only">
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none z-20 ps-4">
-              <svg
-                className="flex-shrink-0 size-4 text-gray-500"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <circle cx="11" cy="11" r="8" />
-                <path d="m21 21-4.3-4.3" />
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="icon"
-              name="icon"
-              className="py-2 pe-4 ps-10 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none sm:w-auto"
-              placeholder="Search"
-            />
-          </div>
-        </div>
+        {postId && <BackButton />}
+
+        {data && !postId && (
+          <Select
+            list={data}
+            isLoading={isLoading}
+            error={error}
+            selectedItem={selectedAutor}
+          />
+        )}
       </nav>
     </header>
   );
