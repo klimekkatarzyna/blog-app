93b77123d27857c3a7b2658569f63863addb1c9d
Katarzyna Krawczyk <katarzynaklimek26@gmail.com>
2024-07-23 10:44:27 +0200

    fix issue with Select component

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,6 +1,5 @@
 import {
   Combobox,
-  ComboboxButton,
   ComboboxInput,
   ComboboxOption,
   ComboboxOptions,
@@ -11,7 +10,6 @@ import { twMerge } from "tailwind-merge";
 
 import { AutorSchemaType } from "../services/autors";
 import { Spinner } from "./icons/Spinner";
-import { AngleDown } from "./icons/AngleDown";
 import { Check } from "./icons/Check";
 
 type SelectProps<T extends AutorSchemaType> = {
@@ -24,11 +22,12 @@ type SelectProps<T extends AutorSchemaType> = {
 export const Select = <T extends AutorSchemaType>({
   list,
   isLoading,
-  error,
   selectedItem,
 }: SelectProps<T>) => {
-  const [query, setQuery] = useState<string>();
-  const [selected, setSelected] = useState<AutorSchemaType | null>(null);
+  const [query, setQuery] = useState<string>("");
+  const [selected, setSelected] = useState<AutorSchemaType | null>(
+    selectedItem || null
+  );
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -37,7 +36,7 @@ export const Select = <T extends AutorSchemaType>({
   }, [selected?.id]);
 
   useEffect(() => {
-    setSelected(selectedItem as AutorSchemaType);
+    setSelected(selectedItem || null);
   }, [setSelected, selectedItem?.id]);
 
   const filteredPeople = useMemo(
@@ -59,52 +58,66 @@ export const Select = <T extends AutorSchemaType>({
     [list, query]
   );
 
-  if (isLoading) {
-    return "loading...";
-  }
-
-  if (error) {
-    return "Soemthing went wrong";
-  }
+  const onClearInput = () => {
+    setQuery("");
+    navigate("/", { replace: true });
+  };
 
   return (
     <Combobox
+      immediate
+      as="div"
       value={selected}
-      onChange={setSelected}
+      onChange={(value) => setSelected(value || null)}
       onClose={() => setQuery("")}
     >
-      <div className="relative">
-        <ComboboxInput
-          className="w-full min-w-[400px] z-30 rounded-lg border-gray-400 border bg-white py-1.5 pr-8 pl-3 text-sm/6 text-gray-900 focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
-          displayValue={(person: T) => person?.name}
-          onChange={(event) => setQuery(event.target.value)}
-        />
-        <ComboboxButton className="group absolute inset-y-0 right-0 px-2.5">
-          <AngleDown />
-        </ComboboxButton>
-      </div>
-      <ComboboxOptions
-        anchor="bottom"
-        transition
-        className="w-[var(--input-width)] cursor-pointer bg-white rounded-xl border border-white p-1 transition duration-100 ease-in"
-      >
-        {isLoading && <Spinner />}
-        {filteredPeople?.map((person) => (
-          <ComboboxOption as={Fragment} key={person.id} value={person}>
-            {({ selected }) => (
-              <div
-                className={twMerge(
-                  "group flex items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-gray-100",
-                  selected ? "bg-gray-100" : ""
-                )}
-              >
-                {selected && <Check />}
-                <Link to={`/posts/userId/${person.id}`}>{person.name}</Link>
-              </div>
-            )}
-          </ComboboxOption>
-        ))}
-      </ComboboxOptions>
+      {({ open }) => (
+        <>
+          <div className="relative">
+            <ComboboxInput
+              className="inline min-w-[400px] z-30 rounded-lg border-gray-400 border bg-white py-1.5 pr-8 pl-3 text-sm/6 text-gray-900 focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+              displayValue={(person: T) => person?.name}
+              onChange={(event) => setQuery(event.target.value)}
+              placeholder="Select autor"
+            />
+
+            <button
+              onClick={() => onClearInput()}
+              type="button"
+              className="bg-gray-300 inline p-2 ml-2 rounded-md transition-all text-gray-700 hover:bg-gray-400 hover:text-white"
+            >
+              Clear input
+            </button>
+          </div>
+          {open && (
+            <ComboboxOptions
+              anchor="bottom"
+              transition
+              className="w-[var(--input-width)] empty:invisible z-50 cursor-pointer bg-white rounded-xl border border-white p-1 transition duration-100 ease-in"
+            >
+              {isLoading && <Spinner />}
+
+              {filteredPeople?.map((person) => (
+                <ComboboxOption as={Fragment} key={person.id} value={person}>
+                  {({ selected }) => (
+                    <div
+                      className={twMerge(
+                        "group flex items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-gray-100",
+                        selected ? "bg-gray-100" : ""
+                      )}
+                    >
+                      {selected && <Check />}
+                      <Link to={`/posts/userId/${person.id}`}>
+                        {person.name}
+                      </Link>
+                    </div>
+                  )}
+                </ComboboxOption>
+              ))}
+            </ComboboxOptions>
+          )}
+        </>
+      )}
     </Combobox>
   );
 };
